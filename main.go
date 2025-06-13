fd70d13c26cdf12fc8e9408b378627aab2b63ee0
rohitkshirsagar19 <rohitkshirsagar1904@gmail.com>
2025-06-13 18:30:29 +0530

    Learned about struct and map

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// GO Data Structure
+type Task struct {
+	ID     int
+	Name   string
+	Status bool
+}
+
 func main() {
 
 	// Variable declaration
@@ -18,6 +25,29 @@ func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println("Number: ", i+1)
 	}
+
+	// Create a Task instance
+	tasks := []Task{
+		{ID: 1, Name: "Write Code", Status: true},
+		{ID: 2, Name: "Review Code", Status: false},
+		{ID: 3, Name: "Test Code", Status: true},
+	}
+
+	// Apend task
+	tasks = append(tasks, Task{ID: 4, Name: "Deploy Code", Status: false})
+
+	// Loop over the tasks
+	for _, task := range tasks {
+		fmt.Printf("ID: %d , Name: %s , Status: %v \n", task.ID, task.Name, task.Status)
+	}
+
+	// Mapping
+	taskStatus := map[int]bool{
+		1: true,
+		0: false,
+	}
+	fmt.Println("Task 1 done?", taskStatus[1])
+
 }
 
 // Function to add two integers
